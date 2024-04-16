538097475f10ad35f35929c9c5ff02ec56c1e329
Talison Amorim <amorimtalison9@gmail.com>
2024-04-15 22:44:39 -0300

    Aula 7 aplicando a IA

--- controls.js
+++ controls.js
@@ -1,6 +1,12 @@
 export default {
-    junp: new keyboardEvent('keydown', {key: 'Space', KeyCode: 32}),
-    dispatch(event){
-        document.dispatchEvent(this[event]);
+    jump: new KeyboardEvent('keydown', { key: ' ', keyCode: 32 }), // Observação: você pode querer usar ' ' em vez de 'Space'
+    
+    dispatch(event) {
+        // Verifique se this[event] existe antes de tentar dispará-lo
+        if (this[event]) {
+            document.dispatchEvent(this[event]);
+        } else {
+            console.warn(`Evento "${event}" não encontrado.`);
+        }
     }
-}
+};
